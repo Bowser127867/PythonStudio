bcae477ec4bc0b26172421f2fb3f0343bb73d538
Bowser127867 <sabriomerakdeniz@gmail.com>
2026-06-16 10:09:39 +0300

    Removal of projects and projects/MyFirstGame (0.3)

--- projects/MyFirstGame/scripts/spin.py
+++
@@ -1,7 +0,0 @@
-class Test:
-    def start(self):
-        print("i am working currently")
-        print(1+1)
-                
-    def update(self, dt):
-        self.object.rotation[1] += 90 * dt
